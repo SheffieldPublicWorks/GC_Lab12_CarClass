40d4883c30dee34b9c85cb62543a44662d89f57f
unknown <stephen.scobie@gmail.com>
2017-11-03 15:39:00 -0400

    Initial commit.

---
+++ GC_Deliverable12_LabAlt10_CarClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Deliverable12_LabAlt10_CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
